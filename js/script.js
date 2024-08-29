c886780c0a54d51528ecea9329916f1e7e052ced
okhj159 <okhj159@gmail.com>
2024-08-29 14:47:11 +0900

    fix: 이벤트 페이지 css js 수정

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ const btn = document.querySelector('.btn-menu');
 const nav = document.querySelector('.main-nav');
 
 btn.addEventListener('click', () => {
-    nav.classList.toggle('open-menu');
-    if(btn.innerHTML === 'Menu') {
-        btn.innerHTML = 'Close';
-    } else {
-        btn.innerHTML = 'Menu';
-    }
-})
+  nav.classList.toggle('open-menu');
+  if (btn.innerHTML === 'Menu') {
+    btn.innerHTML = 'Close';
+  } else {
+    btn.innerHTML = 'Menu';
+  }
+});
