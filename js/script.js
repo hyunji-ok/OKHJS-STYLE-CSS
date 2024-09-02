a222ca4662b0b8ca720b2ee1a78e031b212800b7
okhj159 <okhj159@gmail.com>
2024-09-02 14:09:58 +0900

    fix: 갤러리 사이트 css js 수정

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
+/* Lightbox Luminous */
+// new LuminousGallery(document.querySelectorAll(".grid-gallery"));
 new LuminousGallery(document.querySelectorAll('.grid-gallery'), {}, {
     caption: function(trigger) {
-        return trigger.querySelector('img').getAttribute('alt');
+      return trigger.querySelector('img').getAttribute('alt');
     }
-});
-
-AOS.init({
+  });
+  
+  /* Fadeup Animation AOS */
+  // AOS.init();
+  AOS.init({
     duration: 1000
-})
+  });
